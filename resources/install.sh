1ad73a3e931d1c61381aff03291fbf94a3ebaec0
MatteoDep <matteo.dep97@gmail.com>
2021-09-13 22:28:16 +0200

    lock at suspend

--- resources/install.sh
+++ resources/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 scriptsdir=$(dirname "$(dirname "$(whereis $0 | awk '{print $2}')")")
-echo "$scriptsdir"
 ln -s "$scriptsdir"/resources/icons ~/.local/share/
 sudo ln -sf "$scriptsdir"/fzfmenu /usr/bin/dmenu
 sudo ln -sf "$scriptsdir"/fzfmenu_run /usr/bin/dmenu_run
+sudo ln -sf "$scriptsdir"/resources/lock-at-suspend.sh /usr/lib/elogind/system-sleep/
---
+++ resources/lock-at-suspend.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Lock before suspend integration with elogind
+
+username=matteo
+userhome=/home/$username
+export XAUTHORITY="/run/user/1000/Xauthority"
+export DISPLAY=":1"
+case "${1}" in
+    pre)
+        su $username -c "$userhome/.local/bin/scripts/lock" &
+        sleep 1s;
+        ;;
+esac
