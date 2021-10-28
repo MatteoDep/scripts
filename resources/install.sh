53d37ea1837d13a86734117c5f3b1b7edf758c5b
MatteoDep <matteo.dep97@gmail.com>
2021-10-28 16:44:10 +0200

    some fixes

--- resources/install.sh
+++ resources/install.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
+if sudo 2>/dev/null; then
+    sudo_cmd="sudo"
+else
+    sudo_cmd="doas"
+fi
+
 scriptsdir=$(dirname "$(dirname "$(whereis $0 | awk '{print $2}')")")
-ln -s "$scriptsdir"/resources/icons ~/.local/share/
-sudo ln -sf "$scriptsdir"/fzfmenu /usr/bin/dmenu
-sudo ln -sf "$scriptsdir"/fzfmenu_run /usr/bin/dmenu_run
-sudo ln -sf "$scriptsdir"/resources/lock-at-suspend.sh /usr/lib/elogind/system-sleep/
+bindir="/usr/bin"
+datadir="${XDG_DATA_HOME:-"$HOME/.local/share"}"
+
+if [ "$1" = "-f" ]; then
+    rm "$datadir/icons"
+    $sudo_cmd rm "$bindir/dmenu"
+    $sudo_cmd rm "$bindir/dmenu_run"
+    $sudo_cmd rm /usr/lib/elogind/system-sleep/lock-at-suspend.sh
+fi
+
+cp "$scriptsdir"/resources/icons "$datadir"
+$sudo_cmd cp "$scriptsdir"/fzfmenu "$bindir"/dmenu
+$sudo_cmd cp "$scriptsdir"/fzfmenu_run "$bindir"/dmenu_run
+$sudo_cmd cp "$scriptsdir"/resources/lock-at-suspend.sh /usr/lib/elogind/system-sleep/
--- resources/lock-at-suspend.sh
+++ resources/lock-at-suspend.sh
@@ -7,7 +7,7 @@ export XAUTHORITY="/run/user/1000/Xauthority"
 export DISPLAY=":1"
 case "${1}" in
     pre)
-        su $username -c "$userhome/.local/bin/scripts/lock" &
-        sleep 1s;
+        su $username -c "$userhome/.local/bin/scripts/lock"
+        sleep 1s
         ;;
 esac
